8bbae9a93916d228c5455fee48a722da0d9429f6
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2012-12-30 10:03:44 +0000

    Remodel update page in preferences based on suggestion by Aleksandar Kovac.

--- FileZilla3/trunk/src/interface/settings/optionspage_updatecheck.cpp
+++ FileZilla3/trunk/src/interface/settings/optionspage_updatecheck.cpp
@@ -10,37 +10,54 @@
 
 BEGIN_EVENT_TABLE(COptionsPageUpdateCheck, COptionsPage)
 EVT_BUTTON(XRCID("ID_RUNUPDATECHECK"), COptionsPageUpdateCheck::OnRunUpdateCheck)
-EVT_CHECKBOX(XRCID("ID_CHECKBETA"), COptionsPageUpdateCheck::OnCheckBeta)
 END_EVENT_TABLE()
 
 bool COptionsPageUpdateCheck::LoadPage()
 {
 	bool failure = false;
-	SetCheckFromOption(XRCID("ID_UPDATECHECK"), OPTION_UPDATECHECK, failure);
-	SetCheckFromOption(XRCID("ID_CHECKBETA"), OPTION_UPDATECHECK_CHECKBETA, failure);
-	SetTextFromOption(XRCID("ID_DAYS"), OPTION_UPDATECHECK_INTERVAL, failure);
+	int sel;
+	if (!m_pOptions->GetOptionVal(OPTION_UPDATECHECK))
+		sel = 0;
+	else
+	{
+		int days = m_pOptions->GetOptionVal(OPTION_UPDATECHECK_INTERVAL);
+		if (days <= 7)
+			sel = 1;
+		else if (days <= 14)
+			sel = 2;
+		else
+			sel = 3;
+	}
+	SetChoice(XRCID("ID_UPDATECHECK"), sel, failure);
+
+	SetChoice(XRCID("ID_UPDATETYPE"), (m_pOptions->GetOptionVal(OPTION_UPDATECHECK_CHECKBETA) != 0) ? 1 : 0, failure);
 
 	return !failure;
 }
 
 bool COptionsPageUpdateCheck::SavePage()
 {
-	SetOptionFromCheck(XRCID("ID_UPDATECHECK"), OPTION_UPDATECHECK);
-	SetOptionFromCheck(XRCID("ID_CHECKBETA"), OPTION_UPDATECHECK_CHECKBETA);
-	SetOptionFromText(XRCID("ID_DAYS"), OPTION_UPDATECHECK_INTERVAL);
-
-	return true;
-}
-
-bool COptionsPageUpdateCheck::Validate()
-{
-	unsigned long days;
-	if (!GetText(XRCID("ID_DAYS")).ToULong(&days) || days < 7)
+	int sel = GetChoice(XRCID("ID_UPDATECHECK"));
+	m_pOptions->SetOption(OPTION_UPDATECHECK, (sel > 0) ? 1 : 0);
+	int days = 0;
+	switch (sel)
 	{
-		XRCCTRL(*this, "ID_DAYS", wxTextCtrl)->SetFocus();
-		DisplayError(_T("ID_DAYS"), _("Please enter an update interval of at least 7 days"));
-		return false;
+	case 1:
+		days = 7;
+		break;
+	case 2:
+		days = 14;
+		break;
+	case 3:
+		days = 30;
+		break;
+	default:
+		break;
 	}
+	m_pOptions->SetOption(OPTION_UPDATECHECK_INTERVAL, days);
+
+	int type = GetChoice(XRCID("ID_UPDATETYPE"));
+	m_pOptions->SetOption(OPTION_UPDATECHECK_CHECKBETA, (type > 0) ? 1 : 0);
 
 	return true;
 }
@@ -52,18 +69,6 @@ void COptionsPageUpdateCheck::OnRunUpdateCheck(wxCommandEvent &event)
 		return;
 
 	dlg.Run();
-}
-
-void COptionsPageUpdateCheck::OnCheckBeta(wxCommandEvent& event)
-{
-	if (event.IsChecked())
-	{
-		if (wxMessageBox(_("Do you really want to check for beta versions?\nUnless you want to test new features, keep using stable versions."), _("Update wizard"), wxICON_QUESTION | wxYES_NO, this) != wxYES)
-		{
-			XRCCTRL(*this, "ID_CHECKBETA", wxCheckBox)->SetValue(0);
-			return;
-		}
-	}
 }
 
 #endif //FZ_MANUALUPDATECHECK && FZ_AUTOUPDATECHECK
