ee556fe610b5bfcccf3b8cabe8b861b7e6ab0d22
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2013-06-10 18:29:58 +0000

    Handle a particular case of connection failure earlier.

--- FileZilla3/trunk/src/engine/transfersocket.cpp
+++ FileZilla3/trunk/src/engine/transfersocket.cpp
@@ -165,7 +165,16 @@ void CTransferSocket::OnSocketEvent(CSocketEvent &event)
 	switch (event.GetType())
 	{
 	case CSocketEvent::connection:
-		OnConnect();
+		if (event.GetError())
+		{
+			if (!m_transferEndReason)
+			{
+				m_pControlSocket->LogMessage(::Error, _("The data connection could not be established: %s"), CSocket::GetErrorDescription(event.GetError()).c_str());
+				TransferEnd(transfer_failure);
+			}
+		}
+		else
+			OnConnect();
 		break;
 	case CSocketEvent::read:
 		OnReceive();
