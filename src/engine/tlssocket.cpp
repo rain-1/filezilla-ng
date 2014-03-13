34fd817dcb4f744a86636ac370b7d1e301f4a131
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2014-03-13 07:37:46 +0000

    Use gnutls_session_get_data2 instead of gnutls_session_get_data to simplify the code.

--- src/engine/tlssocket.cpp
+++ src/engine/tlssocket.cpp
@@ -430,31 +430,17 @@ void CTlsSocket::OnSend()
 
 bool CTlsSocket::CopySessionData(const CTlsSocket* pPrimarySocket)
 {
-	size_t session_data_size = 0;
-
-	// Get buffer size
-	int res = gnutls_session_get_data(pPrimarySocket->m_session, 0, &session_data_size);
-	if (res && res != GNUTLS_E_SHORT_MEMORY_BUFFER )
-	{
-		m_pOwner->LogMessage(Debug_Warning, _T("gnutls_session_get_data on primary socket failed: %d"), res);
-		return true;
-	}
-
-	// Get session data
-	char *session_data = new char[session_data_size];
-	res = gnutls_session_get_data(pPrimarySocket->m_session, session_data, &session_data_size);
-	if (res)
-	{
-		delete [] session_data;
-		m_pOwner->LogMessage(Debug_Warning, _T("gnutls_session_get_data on primary socket failed: %d"), res);
+	gnutls_datum_t d;
+	int res = gnutls_session_get_data2(pPrimarySocket->m_session, &d);
+	if (res) {
+		m_pOwner->LogMessage(Debug_Warning, _T("gnutls_session_get_data2 on primary socket failed: %d"), res);
 		return true;
 	}
 
 	// Set session data
-	res = gnutls_session_set_data(m_session, session_data, session_data_size );
-	delete [] session_data;
-	if (res)
-	{
+	res = gnutls_session_set_data(m_session, d.data, d.size );
+	gnutls_free(d.data);
+	if (res) {
 		m_pOwner->LogMessage(Debug_Info, _T("gnutls_session_set_data failed: %d. Going to reinitialize session."), res);
 		UninitSession();
 		if (!InitSession())
