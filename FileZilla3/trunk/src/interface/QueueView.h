887fe76c67011c907836b8cb95e7b6c57b4038ad
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2014-03-24 12:18:48 +0000

    Implement power-related queue completion actions on OS X

--- FileZilla3/trunk/src/interface/QueueView.h
+++ FileZilla3/trunk/src/interface/QueueView.h
@@ -48,8 +48,8 @@ enum ActionAfterState
 	ActionAfterState_RunCommand,
 	ActionAfterState_ShowMessage,
 	ActionAfterState_PlaySound
-// On Windows, wx can reboot or shutdown the system as well.
-#ifdef __WXMSW__
+// On Windows and OS X, wx can reboot or shutdown the system as well.
+#if defined(__WXMSW__) || defined(__WXMAC__)
 	,
 	ActionAfterState_Reboot,
 	ActionAfterState_Shutdown,
@@ -222,7 +222,7 @@ protected:
 
 	bool IsActionAfter(enum ActionAfterState);
 	void ActionAfter(bool warned = false);
-#ifdef __WXMSW__
+#if defined(__WXMSW__) || defined(__WXMAC__)
 	void ActionAfterWarnUser(ActionAfterState s);
 #endif
 
@@ -278,7 +278,7 @@ protected:
 
 	enum ActionAfterState m_actionAfterState;
 	wxString m_actionAfterRunCommand;
-#ifdef __WXMSW__
+#if defined(__WXMSW__) || defined(__WXMAC__)
 	wxTimer* m_actionAfterTimer;
 	wxProgressDialog* m_actionAfterWarnDialog;
 	int m_actionAfterTimerCount;
