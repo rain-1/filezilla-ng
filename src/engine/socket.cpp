5f63f0a14852aa1133ce594b0c31722779bd9b8e
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2014-05-27 06:42:15 +0000

    Bump _WIN32_WINNT.

--- src/engine/socket.cpp
+++ src/engine/socket.cpp
@@ -2,12 +2,12 @@
 #ifdef __WXMSW__
   // MinGW needs this for getaddrinfo
   #if defined(_WIN32_WINNT)
-    #if _WIN32_WINNT < 0x0501
+    #if _WIN32_WINNT < 0x0600
       #undef _WIN32_WINNT
-      #define _WIN32_WINNT 0x0501
+      #define _WIN32_WINNT 0x0600
     #endif
   #else
-    #define _WIN32_WINNT 0x0501
+    #define _WIN32_WINNT 0x0600
   #endif
   #include <winsock2.h>
   #include <ws2tcpip.h>
