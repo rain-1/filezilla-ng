3b2a68290593981cb779fb3272e30ccff9ce19dd
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2014-06-19 15:13:25 +0000

    Further refactoring.

--- FileZilla3/trunk/src/interface/themeprovider.h
+++ FileZilla3/trunk/src/interface/themeprovider.h
@@ -2,6 +2,7 @@
 #define __THEMEPROVIDER_H__
 
 #include <option_change_event_handler.h>
+#include <memory>
 
 enum iconSize
 {
@@ -16,10 +17,10 @@ public:
 	CThemeProvider();
 	virtual ~CThemeProvider();
 
-	static std::list<wxString> GetThemes();
-	static std::list<wxBitmap*> GetAllImages(const wxString& theme, const wxSize& size);
+	static std::vector<wxString> GetThemes();
+	static std::vector<std::unique_ptr<wxBitmap>> GetAllImages(const wxString& theme, const wxSize& size);
 	static bool GetThemeData(const wxString& themePath, wxString& name, wxString& author, wxString& email);
-	static std::list<wxString> GetThemeSizes(const wxString& themePath);
+	static std::vector<wxString> GetThemeSizes(const wxString& themePath);
 	static wxIconBundle GetIconBundle(const wxArtID& id, const wxArtClient& client = wxART_OTHER);
 	static bool ThemeHasSize(const wxString& themePath, const wxString& size);
 
