16d165ca4c8583ef740a8ea136882237b56396a1
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2014-07-02 09:09:42 +0000

    Update script to clean XRC files. Added special option to minify files removing structure.

--- FileZilla3/trunk/src/interface/resources/xrc/xrcclean.sh
+++ FileZilla3/trunk/src/interface/resources/xrc/xrcclean.sh
@@ -1,19 +1,50 @@
 #! /bin/sh
 
-# XRCed has the nasty habit to add empty lines to the xrc files. This small script removes them.
+set -e
 
-if [ -z "$1" ]; then
-  # No argument given, find all .xrc files and call ourself once for each .xrc file
-  find . -name "*.xrc" | xargs ./xrcclean.sh
-else
-  # Remove newlines and lines containing only spaces from all given files
-  while test $# != 0; do
-    cat "$1" | sed -e "s/^ *$//" | sed -e "/^$/d" > "$1_new"
-    mv "$1_new" "$1"
-    if uname | grep MINGW > /dev/null; then
-      unix2dos "$1"
-    fi
-    shift
-  done
+minify=
+if [ "$1" = "-m" ]; then
+  minify=1
+  shift
 fi
 
+do_clean() {
+  infile="$1"
+  if [ -z "$2" ]; then
+    outfile="$1"
+  else
+    outfile="$2"
+  fi
+  tmpfile="${outfile}~"
+
+  if [ "$minify" = "1" ]; then
+    cat "$infile" | sed -e 's/^ *//g' | sed -e 's/ *$//' | tr -d "\r\n" > "$tmpfile"
+    mv "$tmpfile" "$outfile"
+  elif [ "$infile" != "$outfile" ]; then
+    cp "$infile" "$outfile"
+  fi
+
+  # Remove newlines and lines containing only spaces from all given files
+  # Remove empty elements
+  cat "$outfile" | sed -e "s/ *$//" | sed -e "/^$/d" \
+    | sed -e 's/<\([[:alnum:]]\+\) *><\/\1>//g' \
+    | sed -e 's/<\([[:alnum:]]\+\) *\( [^>]\+\)" *><\/\1>/<\1\2"\/>/g' \
+    | sed -e "/^ *$/d" \
+    > "$tmpfile"
+  mv "$tmpfile" "$outfile"
+ 
+  if [ "$minify" != "1" ]; then
+    if uname | grep -i 'MINGW\|CYGWIN' > /dev/null; then
+      unix2dos "$outfile"
+    fi
+  fi
+}
+
+if [ ! -f "$1" ]; then
+  for i in *.xrc; do
+    do_clean "$i" || exit 1
+  done
+  exit
+fi
+
+do_clean "$1" "$2"
