e250e46255bf5833f3b2044a4d402faebe5c3a65
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2014-08-06 08:59:26 +0000

    Fix tab order upon splitting.

--- FileZilla3/trunk/src/interface/splitter.h
+++ FileZilla3/trunk/src/interface/splitter.h
@@ -27,6 +27,8 @@ public:
 	bool Unsplit(wxWindow* toRemove = NULL);
 
 protected:
+	void PrepareSplit(wxWindow* window1, wxWindow* window2, int sashPosition, bool horizontal);
+
 	virtual int OnSashPositionChanging(int newSashPosition);
 
 	int CalcSoftLimit(int newSashPosition);
