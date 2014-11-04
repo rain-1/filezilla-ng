147af3ed1f088930950eb39de3814fa079015d89
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2014-11-04 18:29:15 +0000

    Pass O_CLOEXEC when opening files.

--- src/interface/ipcmutex.cpp
+++ src/interface/ipcmutex.cpp
@@ -17,11 +17,10 @@ CInterProcessMutex::CInterProcessMutex(enum t_ipcMutexType mutexType, bool initi
 	// Create mutex_
 	hMutex = ::CreateMutex(0, false, wxString::Format(_T("FileZilla 3 Mutex Type %d"), mutexType));
 #else
-	if (!m_instanceCount)
-	{
+	if (!m_instanceCount) {
 		// Open file only if this is the first instance
 		wxFileName fn(COptions::Get()->GetOption(OPTION_DEFAULT_SETTINGSDIR), _T("lockfile"));
-		m_fd = open(fn.GetFullPath().mb_str(), O_CREAT | O_RDWR, 0644);
+		m_fd = open(fn.GetFullPath().mb_str(), O_CREAT | O_RDW0R | O_CLOEXEC, 0644);
 	}
 	m_instanceCount++;
 #endif
