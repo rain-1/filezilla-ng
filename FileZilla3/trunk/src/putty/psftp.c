6fc7fea56abd81698d2b03a084975ff1e2fbe543
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2014-11-27 14:04:39 +0000

    Do not assume the mtime field is initialized if the ACMODTIME flag is not present.

--- FileZilla3/trunk/src/putty/psftp.c
+++ FileZilla3/trunk/src/putty/psftp.c
@@ -1265,7 +1265,10 @@ int sftp_cmd_ls(struct sftp_command *cmd)
 	 * And print them.
 	 */
 	for (i = 0; i < nnames; ++i) {
-	    unsigned long long mtime = ((unsigned long long)ournames[i]->attrs.mtime.hi << 32) + ournames[i]->attrs.mtime.lo;
+	    unsigned long long mtime = 0;
+	    if (ournames[i]->attrs.flags & SSH_FILEXFER_ATTR_ACMODTIME) {
+		mtime = ((unsigned long long)ournames[i]->attrs.mtime.hi << 32) + ournames[i]->attrs.mtime.lo;
+	    }
 	    fzprintf(sftpListentry, "%llu %s", mtime, ournames[i]->longname);
 	    fxp_free_name(ournames[i]);
 	}
