034bb1d25d09059aca3abdab526fc430d472a7bd
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-04-22 13:24:38 +0000

    Partially revert 6652, MinGW lacks import library for SHGetKnownFolderPath.

--- FileZilla3/trunk/src/interface/file_utils.cpp
+++ FileZilla3/trunk/src/interface/file_utils.cpp
@@ -4,7 +4,7 @@
 
 #include <wx/stdpaths.h>
 #ifdef __WXMSW__
-#include <shlobj.h>
+#include <wx/dynlib.h> // Used by GetDownloadDir
 #include <knownfolders.h>
 #else
 #include <wx/textfile.h>
@@ -323,8 +323,9 @@ bool RenameFile(wxWindow* parent, wxString dir, wxString from, wxString to)
 #endif
 }
 
-
-#ifndef __WXMSW__
+#ifdef __WXMSW__
+extern "C" typedef HRESULT (WINAPI *tSHGetKnownFolderPath)(const GUID& rfid, DWORD dwFlags, HANDLE hToken, PWSTR *ppszPath);
+#else
 wxString ShellUnescape( wxString const& path )
 {
 	wxString ret;
@@ -345,12 +346,18 @@ wxString ShellUnescape( wxString const& path )
 CLocalPath GetDownloadDir()
 {
 #ifdef __WXMSW__
-	PWSTR path;
-	HRESULT result = SHGetKnownFolderPath(FOLDERID_Downloads, 0, 0, &path);
-	if(result == S_OK) {
-		wxString dir = path;
-		CoTaskMemFree(path);
-		return CLocalPath(dir);
+	// Unfortunately MinGW's import library lacks SHGetKnownFolderPath, even though it has it in its headers.
+	wxDynamicLibrary lib(_T("shell32.dll"));
+	if (lib.IsLoaded() && lib.HasSymbol(_T("SHGetKnownFolderPath"))) {
+		tSHGetKnownFolderPath pSHGetKnownFolderPath = (tSHGetKnownFolderPath)lib.GetSymbol(_T("SHGetKnownFolderPath"));
+
+		PWSTR path;
+		HRESULT result = pSHGetKnownFolderPath(FOLDERID_Downloads, 0, 0, &path);
+		if(result == S_OK) {
+			wxString dir = path;
+			CoTaskMemFree(path);
+			return CLocalPath(dir);
+		}
 	}
 #elif !defined(__WXMAC__)
 	// Code copied from wx, but for downloads directory.
