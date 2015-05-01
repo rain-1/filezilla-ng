f8ec743b71c9a106fc8eb22fec810f4235f57e16
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-05-01 13:58:58 +0000

    Fix a format string vulnerability if MALLOC_LOG is set.

--- src/putty/misc.c
+++ src/putty/misc.c
@@ -739,7 +739,7 @@ void *safemalloc(size_t n, size_t size)
 #else
 	strcpy(str, "Out of memory!");
 #endif
-	modalfatalbox(str);
+	modalfatalbox("%s", str);
     }
 #ifdef MALLOC_LOG
     if (fp)
@@ -781,7 +781,7 @@ void *saferealloc(void *ptr, size_t n, size_t size)
 #else
 	strcpy(str, "Out of memory!");
 #endif
-	modalfatalbox(str);
+	modalfatalbox("%s", str);
     }
 #ifdef MALLOC_LOG
     if (fp)
