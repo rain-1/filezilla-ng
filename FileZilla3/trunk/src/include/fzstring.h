a98f82045e72384c54954570800cb6e2d4eed091
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-08-17 15:48:49 +0000

    Fix *nix builds.

--- FileZilla3/trunk/src/include/fzstring.h
+++ FileZilla3/trunk/src/include/fzstring.h
@@ -5,16 +5,13 @@
 
 #include <string>
 
-#ifdef __WXMSW__
 typedef std::wstring fzstring;
 
 inline fzstring to_fzstring(wxString const& s) { return s.ToStdWstring(); }
+#ifdef __WXMSW__
 inline int collate_fzstring(fzstring const& a, fzstring const& b) { return _wcsicmp(a.c_str(), b.c_str()); } // note: does not handle embedded null
 #else
-typedef std::string fzstring;
-
-inline fzstring to_fzstring(wxString const& s) { return s.ToStdString();  }
-inline int collate_fzstring(fzstring const& a, fzstring const& b) { return strcasecmp(a.c_str(), b.c_str()); } // note: does not handle embedded null
+inline int collate_fzstring(fzstring const& a, fzstring const& b) { return wcscasecmp(a.c_str(), b.c_str()); } // note: does not handle embedded null
 #endif
 
 #endif
