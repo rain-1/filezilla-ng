137e70b5daca8c5dcb00c4354ab2f79533ef1150
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-09-28 14:15:19 +0000

    Add the MinGW implementation for the threads.

--- FileZilla3/trunk/lib/fz_thread.cpp
+++ FileZilla3/trunk/lib/fz_thread.cpp
@@ -10,6 +10,104 @@
 
 namespace fz {
 
+thread::thread()
+{
+}
+
+bool thread::joinable() const
+{
+	return impl_ == 0;
+}
+
+#ifdef USE_CUSTOM_THREADS
+// MinGW is a special snowflake...
+
+namespace {
+class entry_dispatch
+{
+public:
+	virtual ~entry_dispatch() {}
+
+	static void call_entry(entry_dispatch & e)
+	{
+		e.entry();
+	}
+
+	virtual void entry() = 0;
+};
+
+extern "C" static unsigned __stdcall thread_proc(void* arg)
+{
+	entry_dispatch* e = static_cast<entry_dispatch*>(arg);
+	if (e) {
+		entry_dispatch::call_entry(*e);
+	}
+	return 0;
+}
+}
+
+class thread::impl final : entry_dispatch
+{
+public:
+	impl(thread& t);
+
+	thread& t_;
+
+	HANDLE handle_{INVALID_HANDLE_VALUE};
+
+	mutex m_{false};
+
+	virtual void entry();
+};
+
+thread::impl::impl(thread& t)
+	: t_(t)
+{
+}
+
+void thread::impl::entry()
+{
+	{
+		// Obtain mutex once. Once we have it, handle_ is assigned.
+		scoped_lock l(m_);
+	}
+	t_.entry();
+}
+
+bool thread::run()
+{
+	if (impl_) {
+		return false;
+	}
+
+	impl_ = new impl(*this);
+
+	{
+		scoped_lock l(impl_->m_);
+		impl_->handle_ = reinterpret_cast<HANDLE>(_beginthreadex(0, 0, thread_proc, impl_, 0, 0));
+	}
+
+	if (impl_->handle_ == INVALID_HANDLE_VALUE) {
+		delete impl_;
+		impl_ = 0;
+	}
+
+	return impl_ != 0;
+}
+
+void thread::join()
+{
+	if (impl_) {
+		WaitForSingleObject(impl_->handle_, INFINITE);
+		CloseHandle(impl_->handle_);
+		delete impl_;
+		impl_ = 0;
+	}
+}
+
+#else
+
+// Canonical implentation for everyone else
 class thread::impl final
 {
 public:
@@ -17,11 +115,11 @@ public:
 
 	mutex m_{false};
 
-	static void run(thread & t);
+	static void entry(thread & t);
 };
 
 
-void thread::impl::run(thread& t)
+void thread::impl::entry(thread& t)
 {
 	if (!t.impl_) {
 		return;
@@ -35,18 +133,6 @@ void thread::impl::run(thread& t)
 	t.entry();
 }
 
-thread::thread()
-{
-}
-
-thread::~thread()
-{
-	if (!joinable()) {
-		std::abort();
-	}
-	delete impl_;
-}
-
 bool thread::run()
 {
 	if (impl_) {
@@ -57,7 +143,7 @@ bool thread::run()
 		impl_ = new impl;
 		
 		scoped_lock l(impl_->m_);
-		std::thread t(impl::run, std::ref(*this));
+		std::thread t(impl::entry, std::ref(*this));
 		impl_->t_ = std::move(t);
 	}
 	catch (std::exception const&) {
@@ -77,9 +163,14 @@ void thread::join()
 	}
 }
 
-bool thread::joinable() const
+#endif
+
+thread::~thread()
 {
-	return impl_ == 0;
+	if (!joinable()) {
+		std::abort();
+	}
+	delete impl_;
 }
 
 }
