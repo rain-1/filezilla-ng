818b92af9c5b21f98c7e857eecc640cb8589d874
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-09-29 18:04:07 +0000

    Implement string conversion on *nix.

--- FileZilla3/trunk/lib/fz_string.hpp
+++ FileZilla3/trunk/lib/fz_string.hpp
@@ -61,7 +61,6 @@ std::wstring to_wstring(std::string const& in);
 
 // Converts from UTF-8 into wstring
 // Undefined behavior if input string is not valid UTF-8.
-// Does not handle embedded nulls
 std::wstring to_wstring_from_utf8(std::string const& in);
 
 }
