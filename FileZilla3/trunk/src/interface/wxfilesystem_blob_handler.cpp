a3566244be9f9df18f99ecdc75be26b0c3d879f4
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2016-09-09 14:45:24 +0000

    Handle base64 encoded blobs.

--- FileZilla3/trunk/src/interface/wxfilesystem_blob_handler.cpp
+++ FileZilla3/trunk/src/interface/wxfilesystem_blob_handler.cpp
@@ -5,7 +5,8 @@
 
 bool wxFileSystemBlobHandler::CanOpen(wxString const& location)
 {
-	return GetProtocol(location) == _T("blob");
+	auto const proto = GetProtocol(location);
+	return proto == _T("blob") || proto == _T("blob64");
 }
 
 wxString wxFileSystemBlobHandler::FindFirst(wxString const& wildcard, int flags)
@@ -19,28 +20,46 @@ wxString wxFileSystemBlobHandler::FindFirst(wxString const& wildcard, int flags)
 
 wxFSFile* wxFileSystemBlobHandler::OpenFile(wxFileSystem& fs, const wxString& location)
 {
+	unsigned char* buf{};
+	size_t buf_len{};
+
 	auto pos = location.Find(':');
-	if (pos != 4) {
-		return 0;
+	if (pos == 4) {
+		// hex
+		wxString data = location.Mid(pos + 1);
+		if (data.size() % 2) {
+			return 0;
+		}
+
+		wxChar const* str = data.c_str();
+
+		unsigned char* buf = static_cast<unsigned char*>(malloc(data.size() / 2));
+		for (size_t i = 0; i < data.size() / 2; ++i) {
+			buf[i] = static_cast<unsigned char>(fz::hex_char_to_int(str[i * 2]) * 16 + fz::hex_char_to_int(str[i * 2 + 1]));
+		}
+
+		buf_len = data.size() / 2;
 	}
-	wxString data = location.Mid(pos + 1);
-	if (data.size() % 2) {
-		return 0;
+	else if (pos == 6) {
+		// base64
+		std::string data = fz::base64_decode(fz::to_utf8(location.Mid(pos + 1)));
+		if (!data.empty()) {
+			buf = static_cast<unsigned char*>(malloc(data.size()));
+			memcpy(buf, data.c_str(), data.size());
+			buf_len = data.size();
+		}
 	}
 
-	wxChar const* str = data.c_str();
+	if (buf) {
+		// Whoever came up with the API for the wx streams obviously didn't ever use it.
+		// Why else wouldn't it have an ownership-taking constructor?
+		auto stream = new wxMemoryInputStream(0, buf_len);
 
-	unsigned char* buf = static_cast<unsigned char*>(malloc(data.size() / 2));
-	for (size_t i = 0; i < data.size() / 2; ++i) {
-		buf[i] = static_cast<unsigned char>(fz::hex_char_to_int(str[i * 2]) * 16 + fz::hex_char_to_int(str[i * 2 + 1]));
+		// Or why does it expect a buffer allocated with malloc?
+		stream->GetInputStreamBuffer()->SetBufferIO(buf, buf_len, true);
+
+		return new wxFSFile(stream, location, _T(""), _T(""), wxDateTime());
 	}
 
-	// Whoever came up with the API for the wx streams obviously didn't ever use it.
-	// Why else wouldn't it have an ownership-taking constructor?
-	auto stream = new wxMemoryInputStream(0, data.size() / 2); 
-
-	// Or why does it expect a buffer allocated with malloc?
-	stream->GetInputStreamBuffer()->SetBufferIO(buf, data.size() / 2, true);
-
-	return new wxFSFile(stream, location, _T(""), _T(""), wxDateTime());
+	return 0;
 }
