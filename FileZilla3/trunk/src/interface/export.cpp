7dad4ac5e4792496907f1f2e97c194030ab51316
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2016-09-20 13:47:10 +0000

    Implement exporting directory listing filters.

--- FileZilla3/trunk/src/interface/export.cpp
+++ FileZilla3/trunk/src/interface/export.cpp
@@ -4,6 +4,7 @@
 #include "xmlfunctions.h"
 #include "ipcmutex.h"
 #include "queue.h"
+#include "xrc_helper.h"
 
 CExportDialog::CExportDialog(wxWindow* parent, CQueueView* pQueueView)
 	: m_parent(parent), m_pQueueView(pQueueView)
@@ -12,37 +13,48 @@ CExportDialog::CExportDialog(wxWindow* parent, CQueueView* pQueueView)
 
 void CExportDialog::Run()
 {
-	if (!Load(m_parent, _T("ID_EXPORT")))
+	if (!Load(m_parent, _T("ID_EXPORT"))) {
 		return;
+	}
 
-	if (ShowModal() != wxID_OK)
+	if (ShowModal() != wxID_OK) {
 		return;
+	}
 
-	bool sitemanager = XRCCTRL(*this, "ID_SITEMANAGER", wxCheckBox)->GetValue();
-	bool settings = XRCCTRL(*this, "ID_SETTINGS", wxCheckBox)->GetValue();
-	bool queue = XRCCTRL(*this, "ID_QUEUE", wxCheckBox)->GetValue();
+	bool sitemanager = xrc_call(*this, "ID_SITEMANAGER", &wxCheckBox::GetValue);
+	bool settings = xrc_call(*this, "ID_SETTINGS", &wxCheckBox::GetValue);
+	bool queue = xrc_call(*this, "ID_QUEUE", &wxCheckBox::GetValue);
+	bool filters = xrc_call(*this, "ID_FILTERS", &wxCheckBox::GetValue);
 
-	if (!sitemanager && !settings && !queue) {
+	if (!sitemanager && !settings && !queue && !filters) {
 		wxMessageBoxEx(_("No category to export selected"), _("Error exporting settings"), wxICON_ERROR, m_parent);
 		return;
 	}
 
 	wxString str;
-	if (sitemanager && !queue && !settings)
+	if (sitemanager && !queue && !settings && !filters) {
 		str = _("Select file for exported sites");
-	else if (!sitemanager && queue && !settings)
+	}
+	else if (!sitemanager && queue && !settings && !filters) {
 		str = _("Select file for exported queue");
-	else if (!sitemanager && !queue && settings)
+	}
+	else if (!sitemanager && !queue && settings && !filters) {
 		str = _("Select file for exported settings");
-	else
+	}
+	else if (!sitemanager && !queue && !settings && filters) {
+		str = _("Select file for exported filters");
+	}
+	else {
 		str = _("Select file for exported data");
+	}
 
 	wxFileDialog dlg(m_parent, str, wxString(),
 					_T("FileZilla.xml"), _T("XML files (*.xml)|*.xml"),
 					wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
 
-	if (dlg.ShowModal() != wxID_OK)
+	if (dlg.ShowModal() != wxID_OK) {
 		return;
+	}
 
 	CXmlFile xml(dlg.GetPath());
 
@@ -55,8 +67,9 @@ void CExportDialog::Run()
 		auto document = file.Load();
 		if (document) {
 			auto element = document.child("Servers");
-			if (element)
+			if (element) {
 				exportRoot.append_copy(element);
+			}
 		}
 	}
 	if (settings) {
@@ -65,13 +78,26 @@ void CExportDialog::Run()
 		auto document = file.Load();
 		if (document) {
 			auto element = document.child("Settings");
-			if (element)
+			if (element) {
 				exportRoot.append_copy(element);
+			}
 		}
 	}
 
 	if (queue) {
 		m_pQueueView->WriteToFile(exportRoot);
+	}
+
+	if (filters) {
+		CInterProcessMutex mutex(MUTEX_FILTERS);
+		CXmlFile file(wxGetApp().GetSettingsFile(_T("filters")));
+		auto document = file.Load();
+		if (document) {
+			auto element = document.child("Filters");
+			if (element) {
+				exportRoot.append_copy(element);
+			}
+		}
 	}
 
 	xml.Save(true);
