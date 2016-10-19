5b81880832a67321034d38bb9884e2ab39adc356
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2016-10-19 13:32:16 +0000

    Fix assertion due to new items being unexpectedly selected by default.

--- src/interface/queue.cpp
+++ src/interface/queue.cpp
@@ -1052,12 +1052,20 @@ void CQueueViewBase::UpdateSelections_ItemRangeAdded(int added, int count)
 			SetItemState(itemsToSelect.front(), wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
 			itemsToSelect.pop_front();
 		}
-		if (itemsToSelect.empty())
-			SetItemState(item, 0, wxLIST_STATE_SELECTED);
-		else if (itemsToSelect.front() == item)
+		if (itemsToSelect.front() == item) {
 			itemsToSelect.pop_front();
+		}
+		else {
+			SetItemState(item, 0, wxLIST_STATE_SELECTED);
+		}
 
 		itemsToSelect.push_back(item + count);
+		if (item + count < GetItemCount()) {
+			// On generic list controls, new items may be selected by default after
+			// increasing the item count: Internally it sometimes keeps track
+			// of only unselected items.
+			itemsToSelect.push_back(item + count);
+		}
 
 		item = GetNextItem(item, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
 	}
