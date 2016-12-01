f307976f8165c1f28eca43d28162257ec4423f9b
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2016-12-01 09:49:22 +0000

    Create quickconnect bar directly.

--- FileZilla3/trunk/src/interface/quickconnectbar.cpp
+++ FileZilla3/trunk/src/interface/quickconnectbar.cpp
@@ -7,6 +7,10 @@
 #include "loginmanager.h"
 #include "Mainfrm.h"
 #include "asksavepassworddialog.h"
+#include "filezillaapp.h"
+
+#include <wx/bmpbuttn.h>
+#include <wx/statline.h>
 
 BEGIN_EVENT_TABLE(CQuickconnectBar, wxPanel)
 EVT_BUTTON(XRCID("ID_QUICKCONNECT_OK"), CQuickconnectBar::OnQuickconnect)
@@ -31,29 +35,63 @@ CQuickconnectBar::~CQuickconnectBar()
 bool CQuickconnectBar::Create(CMainFrame* pParent)
 {
 	m_pMainFrame = pParent;
-	if (!wxXmlResource::Get()->LoadPanel(this, pParent, _T("ID_QUICKCONNECTBAR")))
-	{
-		wxLogError(_("Cannot load Quickconnect bar from resource file"));
+
+	if (!wxPanel::Create(pParent, XRCID("ID_QUICKCONNECT"))) {
 		return false;
 	}
+
+	auto sizer = new wxBoxSizer(wxVERTICAL);
+	SetSizer(sizer);
+#ifndef __WXMAC__
+	sizer->Add(new wxStaticLine(this, -1, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL), wxSizerFlags().Expand());
+#endif
+	auto mainSizer = new wxFlexGridSizer(1, 0, 0, 5);
+	sizer->Add(mainSizer, wxSizerFlags().Border(wxALL, ConvertDialogToPixels(wxPoint(2, 0)).x));
+
+	mainSizer->Add(new wxStaticText(this, -1, _("&Host:")), wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL));
+	m_pHost = new wxTextCtrl(this, -1, wxString(), wxDefaultPosition, ConvertDialogToPixels(wxSize(63, -1)), wxTE_PROCESS_ENTER);
+	m_pHost->SetToolTip(_("Enter the address of the server. To specify the server protocol, prepend the host with the protocol identifier.If no protocol is specified, the default protocol(ftp://) will be used. You can also enter complete URLs in the form protocol://user:pass@host:port here, the values in the other fields will be overwritten then.\n\nSupported protocols are:\n- ftp:// for normal FTP with optional encryption\n- sftp:// for SSH file transfer protocol\n- ftps:// for FTP over TLS (implicit)\n- ftpes:// for FTP over TLS (explicit)"));
+	mainSizer->Add(m_pHost, wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL).Border(wxRIGHT, 5));
+
+	mainSizer->Add(new wxStaticText(this, -1, _("&Username:")), wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL));
+	m_pUser = new wxTextCtrl(this, -1, wxString(), wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER);
+	mainSizer->Add(m_pUser, wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL).Border(wxRIGHT, 5));
+
+	mainSizer->Add(new wxStaticText(this, -1, _("Pass&word:")), wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL));
+	m_pPass = new wxTextCtrl(this, -1, wxString(), wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER|wxTE_PASSWORD);
+	mainSizer->Add(m_pPass, wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL).Border(wxRIGHT, 5));
+
+	mainSizer->Add(new wxStaticText(this, -1, _("&Port:")), wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL));
+	m_pPort = new wxTextCtrl(this, -1, wxString(), wxDefaultPosition, ConvertDialogToPixels(wxSize(27, -1)), wxTE_PROCESS_ENTER);
+	m_pPort->SetToolTip(_("Enter the port on which the server listens. The default for FTP is 21, the default for SFTP is 22."));
+	m_pPort->SetMaxLength(5);
+	mainSizer->Add(m_pPort, wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL).Border(wxRIGHT, 5));
+
+	auto connectSizer = new wxBoxSizer(wxHORIZONTAL);
+	mainSizer->Add(connectSizer, wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL));
+	auto connect = new wxButton(this, XRCID("ID_QUICKCONNECT_OK"), _("&Quickconnect"));
+	connect->SetDefault();
+	connectSizer->Add(connect, wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL));
+
+	wxBitmap bmp(wxGetApp().GetResourceDir().GetPath() + _T("dropdown.png"), wxBITMAP_TYPE_PNG);
+	auto flags = wxSizerFlags().Expand().Align(wxALIGN_CENTER_VERTICAL);
+#if defined(__WXMSW__)
+	wxSize dropdownSize = ConvertDialogToPixels(wxSize(12, -1));
+#elif defined(__WXXMAC__)
+	wSize dropdownSize(20, -1);
+	flags.Border(wxLEFT, 6);
+#else
+	wxSize dropdownSize(-1, -1);
+#endif
+	connectSizer->Add(new wxBitmapButton(this, XRCID("ID_QUICKCONNECT_DROPDOWN"), bmp, wxDefaultPosition, dropdownSize, wxBU_AUTODRAW | wxBU_EXACTFIT), flags);
 
 #ifdef __WXMAC__
 	// Under OS X default buttons are toplevel window wide, where under Windows / GTK they stop at the parent panel.
 	wxTopLevelWindow *tlw = dynamic_cast<wxTopLevelWindow*>(wxGetTopLevelParent(pParent));
-	if (tlw)
+	if (tlw) {
 		tlw->SetDefaultItem(0);
-#endif
-
-	m_pHost = XRCCTRL(*this, "ID_QUICKCONNECT_HOST", wxTextCtrl);
-	m_pUser = XRCCTRL(*this, "ID_QUICKCONNECT_USER", wxTextCtrl);
-	m_pPass = XRCCTRL(*this, "ID_QUICKCONNECT_PASS", wxTextCtrl);
-	m_pPort = XRCCTRL(*this, "ID_QUICKCONNECT_PORT", wxTextCtrl);
-
-	if (!m_pHost || !m_pUser || !m_pPass || !m_pPort)
-	{
-		wxLogError(_("Cannot load Quickconnect bar from resource file"));
-		return false;
 	}
+#endif
 
 	return true;
 }
@@ -61,8 +99,7 @@ bool CQuickconnectBar::Create(CMainFrame* pParent)
 void CQuickconnectBar::OnQuickconnect(wxCommandEvent& event)
 {
 	CState* pState = CContextManager::Get()->GetCurrentContext();
-	if (!pState || !pState->m_pEngine)
-	{
+	if (!pState || !pState->m_pEngine) {
 		wxMessageBoxEx(_("FTP Engine not initialized, can't connect"), _("FileZilla Error"), wxICON_EXCLAMATION);
 		return;
 	}
@@ -93,28 +130,35 @@ void CQuickconnectBar::OnQuickconnect(wxCommandEvent& event)
 	case UNKNOWN:
 		if (CServer::GetProtocolFromPort(server.GetPort()) != FTP &&
 			CServer::GetProtocolFromPort(server.GetPort()) != UNKNOWN)
+		{
 			host = _T("ftp://") + host;
+		}
 		break;
 	default:
 		{
 			const wxString prefix = server.GetPrefixFromProtocol(protocol);
-			if (!prefix.empty())
+			if (!prefix.empty()) {
 				host = prefix + _T("://") + host;
+			}
 		}
 		break;
 	}
 
 	m_pHost->SetValue(host);
-	if (server.GetPort() != server.GetDefaultPort(server.GetProtocol()))
+	if (server.GetPort() != server.GetDefaultPort(server.GetProtocol())) {
 		m_pPort->SetValue(wxString::Format(_T("%d"), server.GetPort()));
-	else
-		m_pPort->SetValue(_T(""));
+	}
+	else {
+		m_pPort->ChangeValue(wxString());
+	}
 
 	m_pUser->SetValue(server.GetUser());
-	if (server.GetLogonType() != ANONYMOUS)
+	if (server.GetLogonType() != ANONYMOUS) {
 		m_pPass->SetValue(server.GetPass());
-	else
-		m_pPass->SetValue(_T(""));
+	}
+	else {
+		m_pPass->ChangeValue(wxString());
+	}
 
 	if (protocol == HTTP || protocol == HTTPS) {
 		wxString protocolError = _("Invalid protocol specified. Valid protocols are:\nftp:// for normal FTP with optional encryption,\nsftp:// for SSH file transfer protocol,\nftps:// for FTP over TLS (implicit) and\nftpes:// for FTP over TLS (explicit).");
@@ -122,11 +166,13 @@ void CQuickconnectBar::OnQuickconnect(wxCommandEvent& event)
 		return;
 	}
 
-	if (event.GetId() == 1)
+	if (event.GetId() == 1) {
 		server.SetBypassProxy(true);
+	}
 
-	if (server.GetLogonType() != ANONYMOUS && !CAskSavePasswordDialog::Run(this))
+	if (server.GetLogonType() != ANONYMOUS && !CAskSavePasswordDialog::Run(this)) {
 		return;
+	}
 
 	if (COptions::Get()->GetOptionVal(OPTION_DEFAULT_KIOSKMODE) && server.GetLogonType() == NORMAL) {
 		server.SetLogonType(ASK);
@@ -154,8 +200,7 @@ void CQuickconnectBar::OnQuickconnectDropdown(wxCommandEvent&)
 	pMenu->Append(3, _("Clear history"));
 
 	m_recentServers = CRecentServerList::GetMostRecentServers();
-	if (!m_recentServers.empty())
-	{
+	if (!m_recentServers.empty()) {
 		pMenu->AppendSeparator();
 
 		unsigned int i = 0;
@@ -168,8 +213,9 @@ void CQuickconnectBar::OnQuickconnectDropdown(wxCommandEvent&)
 			pMenu->Append(10 + i, name);
 		}
 	}
-	else
+	else {
 		pMenu->Enable(3, false);
+	}
 
 	XRCCTRL(*this, "ID_QUICKCONNECT_DROPDOWN", wxButton)->PopupMenu(pMenu);
 	delete pMenu;
