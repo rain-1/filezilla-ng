7871a362e89fcada3b3348c9d99ff61cd01eaeb0
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2017-03-13 09:08:01 +0000

    Revert a previous change, it was actually working already.

--- FileZilla3/trunk/src/interface/power_management_osx.m
+++ FileZilla3/trunk/src/interface/power_management_osx.m
@@ -1,18 +1,16 @@
 #include <Foundation/NSProcessInfo.h>
 
-NSProcessInfo * processInfo;
-
 void const* PowerManagmentImpl_SetBusy()
 {
 	NSActivityOptions opt = NSActivityUserInitiated | NSActivityIdleSystemSleepDisabled;
-	id activity = [processInfo beginActivityWithOptions:NSActivityUserInitiated reason:@"Preventing idle sleep during transfers and other operations."];
+	id activity = [[NSProcessInfo processInfo] beginActivityWithOptions:NSActivityUserInitiated reason:@"Preventing idle sleep during transfers and other operations."];
 	return CFBridgingRetain(activity);
 }
 
 void PowerManagmentImpl_SetIdle(void* activity)
 {
 	if (activity) {
-		id activityId = CFBridgingRelease(activity);
+		id<NSObject> activityId = CFBridgingRelease(activity);
 		[[NSProcessInfo processInfo] endActivity:activityId];
 	}
 }
