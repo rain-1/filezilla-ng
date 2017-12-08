770c68fc0d7267294a264026fd16506f439a31f9
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2017-12-08 10:27:55 +0000

    Revert accidental commit

--- src/interface/window_state_manager.h
+++ src/interface/window_state_manager.h
@@ -19,15 +19,15 @@ public:
 	// Set nonzero if Restore maximized the window.
 	// Reason is that under wxGTK, maximization request may take some time.
 	// It is actually done asynchronously by the window manager.
-	unsigned int m_maximize_requested{};
+	unsigned int m_maximize_requested;
 #endif
 
 protected:
-	bool ReadDefaults(const unsigned int optionId, int& maximized, wxPoint& position, wxSize& size);
+	bool ReadDefaults(const unsigned int optionId, bool& maximized, wxPoint& position, wxSize& size);
 
 	wxTopLevelWindow* m_pWindow;
 
-	int m_lastMaximized{};
+	bool m_lastMaximized;
 	wxPoint m_lastWindowPosition;
 	wxSize m_lastWindowSize;
 
