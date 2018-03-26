bcef5482700853eaaa0021905846e23048e7fd11
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2018-03-26 08:34:57 +0000

    Use the input thread improvements also for reading from fzstorj.

--- src/engine/storj/input_thread.cpp
+++ src/engine/storj/input_thread.cpp
@@ -32,101 +32,108 @@ std::wstring CStorjInputThread::ReadLine(std::wstring &error)
 	char buffer[buffersize];
 
 	while (true) {
-		char c;
-		int read = process_.read(&c, 1);
-		if (read != 1) {
-			if (!read) {
-				error = L"Unexpected EOF.";
-			}
-			else {
-				error = L"Unknown error reading from process";
-			}
+		if (!readFromProcess(error, true)) {
 			return std::wstring();
 		}
 
-		if (c == '\n') {
-			break;
+		auto const* p = recv_buffer_.get();
+		size_t i;
+		for (i = 0; i < recv_buffer_.size(); ++i) {
+			unsigned char const c = p[i];
+
+			if (c == '\n') {
+				recv_buffer_.consume(i + 1);
+
+				while (len && buffer[len - 1] == '\r') {
+					--len;
+				}
+
+				std::wstring const line = owner_.ConvToLocal(buffer, len);
+				if (len && line.empty()) {
+					error = L"Failed to convert reply to local character set.";
+				}
+
+				return line;
+			}
+
+			if (len == buffersize - 1) {
+				// Cap string length
+				continue;
+			}
+
+			buffer[len++] = c;
 		}
-
-		if (len == buffersize - 1) {
-			// Cap string length
-			continue;
-		}
-
-		buffer[len++] = c;
+		recv_buffer_.clear();
 	}
 
-	while (len && buffer[len - 1] == '\r') {
-		--len;
+	return std::wstring();
+}
+
+void CStorjInputThread::processEvent(sftpEvent eventType, std::wstring &error)
+{
+	int lines{};
+	switch (eventType)
+	{
+	case storjEvent::count:
+	case storjEvent::Unknown:
+		error = fz::sprintf(L"Unknown eventType %d", readType);
+		break;
+	case storjEvent::Done:
+	case storjEvent::Recv:
+	case storjEvent::Send:
+	case storjEvent::UsedQuotaRecv:
+	case storjEvent::UsedQuotaSend:
+		break;
+	case storjEvent::Reply:
+	case storjEvent::Error:
+	case storjEvent::ErrorMsg:
+	case storjEvent::Verbose:
+	case storjEvent::Info:
+	case storjEvent::Status:
+	case storjEvent::Transfer:
+		lines = 1;
+		break;
+	case storjEvent::Listentry:
+		lines = 4;
+		break;
+	};
+
+	auto msg = new CStorjEvent;
+	auto & message = std::get<0>(msg->v_);
+	message.type = eventType;
+	for (int i = 0; i < lines && error.empty(); ++i) {
+		message.text[i] = ReadLine(error);
 	}
 
-	std::wstring const line = owner_.ConvToLocal(buffer, len);
-	if (len && line.empty()) {
-		error = L"Failed to convert reply to local character set.";
+	if (!error.empty()) {
+		delete msg;
+		return;
 	}
 
-	return line;
+	owner_.send_event(msg);
 }
 
 void CStorjInputThread::entry()
 {
 	std::wstring error;
 	while (error.empty()) {
-		char readType = 0;
-		int read = process_.read(&readType, 1);
-		if (read != 1) {
+		if (!readFromProcess(error, false)) {
 			break;
 		}
 
+		unsigned char readType = *recv_buffer_.get();
+		recv_buffer_.consume(1);
+
 		readType -= '0';
 
-		if (readType < 0 || readType >= static_cast<char>(storjEvent::count) ) {
+		if (readType >= static_cast<unsigned char>(storjEvent::count) ) {
 			error = fz::sprintf(L"Unknown eventType %d", readType);
 			break;
 		}
 
 		storjEvent eventType = static_cast<storjEvent>(readType);
 
-		int lines{};
-		switch (eventType)
-		{
-		case storjEvent::count:
-		case storjEvent::Unknown:
-			error = fz::sprintf(L"Unknown eventType %d", readType);
-			break;
-		case storjEvent::Done:
-		case storjEvent::Recv:
-		case storjEvent::Send:
-		case storjEvent::UsedQuotaRecv:
-		case storjEvent::UsedQuotaSend:
-			break;
-		case storjEvent::Reply:
-		case storjEvent::Error:
-		case storjEvent::ErrorMsg:
-		case storjEvent::Verbose:
-		case storjEvent::Info:
-		case storjEvent::Status:
-		case storjEvent::Transfer:
-			lines = 1;
-			break;
-		case storjEvent::Listentry:
-			lines = 4;
-			break;
-		};
-
-		auto msg = new CStorjEvent;
-		auto & message = std::get<0>(msg->v_);
-		message.type = eventType;
-		for (int i = 0; i < lines && error.empty(); ++i) {
-			message.text[i] = ReadLine(error);
-		}
-
-		if (!error.empty()) {
-			delete msg;
-			break;
-		}
-
-		owner_.send_event(msg);
+		processEvent(eventType, error);
 	}
 
 	owner_.send_event<StorjTerminateEvent>(error);
--- src/engine/storj/input_thread.h
+++ src/engine/storj/input_thread.h
@@ -3,6 +3,7 @@
 
 class CStorjControlSocket;
 
+#include <libfilezilla/buffer.hpp>
 #include <libfilezilla/thread_pool.hpp>
 
 namespace fz {
@@ -19,14 +20,19 @@ public:
 
 protected:
 
+	bool readFromProcess(std::wstring & error, bool eof_is_error);
 	std::wstring ReadLine(std::wstring &error);
 
 	void entry();
+
+	void processEvent(sftpEvent eventType, std::wstring & error);
 
 	fz::process& process_;
 	CStorjControlSocket& owner_;
 
 	fz::async_task thread_;
+
+	fz::buffer recv_buffer_;
 };
 
 #endif
